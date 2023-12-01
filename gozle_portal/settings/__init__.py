a2333b7fe1ea6400919cf84e4868cbb33174f9a1
Rowsen <bayramovw0@gmail.com>
2023-12-02 04:24:36 +0500

    added dotenv loader

--- gozle_portal/settings/__init__.py
+++ gozle_portal/settings/__init__.py
@@ -2,6 +2,10 @@ from __future__ import absolute_import
 
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables from .env
+
 environ = os.getenv("DJANGO_SETTINGS", "local")
 
 if environ.endswith("local"):
